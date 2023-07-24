9deab62dc966b14f55572bb35898b5a797ca9a03
Filipe Braga <filipe.b.braga@gmail.com>
2023-07-24 18:13:42 -0300

    running cli

---
+++ cli-commands/dynamic-evaluation.js
@@ -0,0 +1,9 @@
+// https://nodejs.org/api/cli.html
+
+// Evaluation is the process of running a bit of code to it's result.
+
+// node --eval "1+1"
+// -e, --eval "script" -> Evaluate the following argument as JavaScript. The modules which are predefined in the REPL can also be used in script.\
+
+// node --print "1+1"
+// -p, --print "script" -> Identical to -e but prints the result.
