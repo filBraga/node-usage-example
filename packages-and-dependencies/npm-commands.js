bfc081d20c090781852388955c5703a3448e499d
Filipe Braga <filipe.b.braga@gmail.com>
2023-07-31 16:42:16 -0300

    packages and dependencies

---
+++ packages-and-dependencies/npm-commands.js
@@ -0,0 +1,10 @@
+// npm init
+// The npm init command can be used to quickly create a package.json in whatever directory it's called in.
+
+// npm install pino
+// install a module in the dependencies
+
+// npm install --save-dev standard
+// install a module in the devDependencies
+
+// There are two package scripts namespaces that have dedicated npm commands: npm test and npm start.
---
+++ packages-and-dependencies/semver.js
@@ -0,0 +1,5 @@
+// Semver versions
+
+// Specifically, ^1.1.1 indicates that you want to use any version that is compatible with 1.1.1, up to, but not including, the next major version.
+
+// ^1.1.1 means any version that is 1.1.1 or higher but less than 2.0.0. It allows for updates within the same major version, so it will include versions like 1.1.2, 1.2.0, 1.3.5, etc., but not versions like 2.0.0 or 1.0.0.
